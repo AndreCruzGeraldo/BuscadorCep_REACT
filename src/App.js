0503f574995bf50ac2c3f2b1570b10d17e4e4b88
André Geraldo <andrecruz2007@hotmail.com>
2024-05-29 14:26:48 -0300

    :rocket: Buscador de Cep

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import './styles.css';
+import api from './services/api';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+
+  function handleInputChange(e) {
+    const value = e.target.value;
+    // Remove caracteres não numéricos
+    const numericValue = value.replace(/\D/g, '');
+    // Adiciona o traço se o valor tiver mais de 5 dígitos
+    let formattedValue = numericValue;
+    if (numericValue.length > 5) {
+      formattedValue = numericValue.slice(0, 5) + '-' + numericValue.slice(5, 8);
+    }
+    // Limita o valor a 9 caracteres (8 dígitos + 1 traço)
+    if (formattedValue.length <= 9) {
+      setInput(formattedValue);
+    }
+  }
+
+  async function handleSearch() {
+    const cepRegex = /^\d{5}-\d{3}$/;
+    if (!cepRegex.test(input)) {
+      alert("Preencha com um CEP válido.");
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input.replace('-', '')}/json`);
+      setCep(response.data);
+      setInput("");
+    } catch {
+      alert("Ops, erro ao buscar.");
+      setInput("");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu cep..."
+          value={input}
+          onChange={handleInputChange}
+        />
+
+        <button className='buttonSearch' onClick={handleSearch}>
+          <FiSearch size={25} color='#FFF' />
+        </button>
+      </div>
+
+      {Object.keys(cep).length > 0 && (
+        <main className='main'>
+          <h2>CEP: {cep.cep}</h2>
+          <span>{cep.logradouro}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+        </main>
+      )}
     </div>
   );
 }
